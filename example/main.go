7935018c62d39d8b8444620add811f1817cc06d6
Jacob Gadikian <jacobgadikian@gmail.com>
2021-08-27 23:27:08 +0700

    fix example`

--- example/main.go
+++ example/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/notional-labs/get"
 )
 
 func main() {
-	get.Get("/Users/faddat/faddatget/example/genesis.json", "Qmc54DreioPpPDUdJW6bBTYUKepmcPsscfqsfFcFmTaVig")
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("cannot get pwd")
+	}
+	joiner := []string{dir, "genesis.json"}
+	path := strings.Join(joiner, "")
+	get.Get(path, "Qmc54DreioPpPDUdJW6bBTYUKepmcPsscfqsfFcFmTaVig")
 }
